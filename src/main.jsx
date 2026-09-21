204e9ae63542a26ed9d97609cfb3ec68b9286ca2
skdigital1710 <skdigitalservice1710@gmail.com>
2026-09-21 12:48:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Admin from "./Admin";
 import "./styles.css";
+
+const isAdminPage = window.location.pathname === "/admin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isAdminPage ? <Admin /> : <App />}
   </React.StrictMode>
 );
